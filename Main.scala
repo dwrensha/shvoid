4c8287cd2e7cd1b8169ff76bc24452e12f34944c
David Renshaw <dwrenshaw@gmail.com>
2011-03-29 18:56:46 -0400

    oops forgot to add this file

---
+++ Main.scala
@@ -0,0 +1,9 @@
+import processing.core.PApplet
+
+object Main {
+  def main (args : Array[String]) : Unit = {
+    println("hello")
+    PApplet.main(Array[String] { "Physics" })
+    println("goodbye")
+  }
+}
